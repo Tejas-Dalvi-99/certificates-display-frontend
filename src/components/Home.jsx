72a9ce152e36628998b7ecb1821fbedeca3e5660
Tejas <tejasdalvi124b@gmail.com>
2025-08-05 18:56:42 +0530

    skeleton loader added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,10 +21,18 @@ function Home() {
 
       <div className='content'>
       <div className='certificates-grid'>
-          {
+          { certificates.length > 0 ? (
             certificates.map((certificate,id)=>{return <div className='certificate-box' key={id}>
               <img src={certificate.img} alt='certificate'></img>
             </div>})
+          ) : 
+          (
+            <div className='certificates-grid'>
+                {Array.from({ length: 8 }, (_, i) => (
+                  <div className="skeleton" key={i} style={{height: '17vw', width: '24vw', borderRadius: '8px', margin: '10px'}}></div>
+                ))}
+            </div>
+          )
           }
       </div>
     </div>
